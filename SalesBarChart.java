c27f29b3d2f867943ba21cd2b58255f2c517dab9
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-08 14:41:35 -0400

    lab 2

--- SalesBarChart.java
+++ SalesBarChart.java
@@ -1,5 +1,14 @@
 package Labs.Week_1.scr;
+import java.util.Scanner;
 
-public class SalesBarChart {
+public class SalesBarChart 
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        
+
+    }
     
 }
