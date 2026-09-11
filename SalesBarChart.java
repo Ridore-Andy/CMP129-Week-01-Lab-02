94e98d81792d018315eacbed855516c8b52ad082
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-10 20:22:41 -0400

    Complete Week 1 Lab 2

--- SalesBarChart.java
+++ SalesBarChart.java
@@ -7,7 +7,28 @@ public class SalesBarChart
     {
         Scanner keyboard = new Scanner(System.in);
 
+        int[] sales = new int[5];
+        //int stores = 0;
+
+        for(int i=0; i<5; i++)
+        {
+            System.out.println("Enter today's sales for store "+(i+1)+": ");
+            sales[i] = keyboard.nextInt();
+        }
         
+        System.out.println("\nSales Bar Chart");
+        System.out.println("Each * = $100");
+
+        for(int i=0; i<5; i++)
+        {
+            int ast = (int)(sales[i]/100);
+            System.out.print("\nStore "+(i+1)+": ");
+            for(int j=0; j<ast; j++)
+            {
+                System.out.print("*");
+            }
+        }
+        keyboard.close();
 
     }
     
