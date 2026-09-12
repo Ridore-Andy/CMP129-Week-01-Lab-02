7410b6a36e640fd83af30d414e57ae543574b004
Ridore-Andy <ridore.andy@student.ccm.edu>
2026-09-11 21:22:56 -0400

    Complete Week 1 Lab 2

--- Patterns.java
+++ Patterns.java
@@ -1,5 +1,29 @@
 package Labs.Week_1.scr;
 
-public class Patterns {
+public class Patterns 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Pattern A");
+        for(int i=1; i<=10; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("+");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nPattern B");
+        for(int i=10; i>=1; i--)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("+");
+            }
+            System.out.println();
+        }
+       
+    }
     
 }
